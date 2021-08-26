56efe07ff80258bc3f082e671421bf978891404d
Ashish Tripathi <ashish.tripathi9@gmail.com>
2021-08-26 12:44:50 +0530

    Create Exception_Subclass.cpp
    showing how to specifically handle catches of subclasses.

---
+++ Exception_Subclass.cpp
@@ -0,0 +1,56 @@
+/* throwing exceptions and catching them 
+we need to be sure that the sub classes are caught first then the parent classes 
+as if the parent class is caught first it will not thow the right exception what we need it to throw
+everything of child class will go to parent and specifics of the error will not be handled.
+
+for example below if catch of execpion() is done before catch of bad_alloc we will not be able to make specific actions in the catch of bad_alloc.
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+class myexception
+{
+    public:
+    void exceptionBad_alloc()
+    {
+        cout<<"throwing bad_alloc exception :"<<endl;
+        throw bad_alloc();
+    }
+    void baseExceptionThorw()
+    {
+        cout<<"thorw base exception class : "<<endl;
+        throw exception();
+    }
+    /*
+    virtual const char* what()
+    {
+        return "returning what() from myexception";
+    }
+    */
+};
+int main()
+{
+    myexception ex;
+    try
+    {
+        ex.exceptionBad_alloc();
+        ex.baseExceptionThorw();
+    }
+    //bad_alloc is the child class of exception class, 
+    //so bad_alloc must be caught before its base class which is exception class
+    catch(bad_alloc &e) 
+    {
+        cout<<"always catch subclass before parent class exception : "<<e.what()<<endl;
+    }
+    //exception class is the parent of all exception classes like bad_alloc etc.
+    catch(exception &e)
+    {
+        cout<<"catching parent or base exception() from base exception class: "<<e.what()<<endl;
+    }
+    
+    cout<<"Program is still running .... ";
+    return 0;
+}
